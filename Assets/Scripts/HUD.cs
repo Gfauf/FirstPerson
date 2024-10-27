719233a196c5862fda69aad6dc8d5a8c8f795626
Gfauf <gaffulau@hotmail.com>
2024-10-27 01:16:52 -0300

    Game over condition and restart

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 
 public class HUD : MonoBehaviour
 {
     public TMP_Text bulletCount;
     public TMP_Text enemiesLeft;
+    public TMP_Text timeLeft;
+    public GameObject gameOverImage;
+
     public static int enemyCount;
+
+    public float targetTime;
+
+    bool gameLost = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +30,13 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        targetTime -= Time.deltaTime;
         UIText();
+        if (targetTime <= 0)
+        {
+            gameLost = true;
+            TimeEnded();
+        }
     }
 
 
@@ -30,5 +44,18 @@ public class HUD : MonoBehaviour
     {
         bulletCount.text = "Bullets: " + ObjectSpawner.bullets.ToString();
         enemiesLeft.text = "Enemies left: " + enemyCount;
+        timeLeft.text = "Time left: " + Mathf.Round(targetTime);
+    }
+
+    public void TimeEnded()
+    {
+        gameOverImage.SetActive(true);
+        Time.timeScale = 0;
+        if (gameLost && Input.GetKeyDown(KeyCode.R))
+        {
+            gameLost = false;
+            Time.timeScale = 1;
+            SceneManager.LoadScene(0);
+        }
     }
 }
